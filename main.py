55a0f9f2ebd778f7f6fefcd27a5443858ee03cd0
Migue <miguelangelmutti@hotmail.com>
2025-12-04 14:28:35 -0300

    agrego logs a main

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from ingestion import AsyncAPIConsumer
 import asyncio
 from config import MAX_CONCURRENCY, TOTAL_REQUESTS, MOVIES_CHANGES_URL, API_KEY, headers
+from utils.logger import setup_logger
 
 if __name__ == "__main__":
+    logger = setup_logger(__name__)
     consumer = AsyncAPIConsumer(MAX_CONCURRENCY, TOTAL_REQUESTS, MOVIES_CHANGES_URL, API_KEY, headers)
+    logger.info("Iniciando consumo de API...")
     asyncio.run(consumer.run())
