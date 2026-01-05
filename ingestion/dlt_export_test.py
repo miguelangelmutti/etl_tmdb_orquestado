f6df8511a08c7c35a26d5bcb27a25b5b014b7964
Migue <miguelangelmutti@hotmail.com>
2026-01-05 16:17:59 -0300

    agrego validacion de esquemas para dlt, corrijo nombre del logger, agrego al endpoint el append de credits

--- ingestion/dlt_export_test.py
+++ ingestion/dlt_export_test.py
@@ -2,15 +2,14 @@ import dlt
 import gzip
 from datetime import datetime
 from dlt.sources.helpers import requests as dlt_requests
+from requests.exceptions import HTTPError
 from dlt.destinations import duckdb
 import sys
 import os
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from config import MOVIE_URL, DAILY_EXPORT_BASE_URL, TEST_DAILY_EXPORT_BASE_URL, DB_PATH, BASE_DIR, LOG_FILE, MOVIE_URL
+from config import MOVIE_URL, DAILY_EXPORT_BASE_URL, TEST_DAILY_EXPORT_BASE_URL, DB_PATH, BASE_DIR, LOG_FILE, MOVIE_URL,DLT_SCHEMA_PATH
 from utils.logger import setup_logger
-
-setup_logger(LOG_FILE)
 
 # 1. EL RECURSO PADRE (El archivo comprimido)
 # Este recurso no escribe en la BD (selected=False) porque solo sirve de "alimentador"
@@ -32,9 +31,9 @@ def tmdb_daily_ids_stream(entity_type="movie"):
 # data_from=tmdb_daily_ids_stream vincula este paso con el anterior
 @dlt.transformer(
     data_from=tmdb_daily_ids_stream, 
-    write_disposition="merge", 
+    write_disposition="append", 
     primary_key="id",
-    table_name="movies_enriched"
+    table_name="tmdb_movies"
 )
 def fetch_movie_details(record, api_key=dlt.secrets["tmdb_access_token"]):
     # ESTRATEGIA DE FILTRADO (CRÍTICO)
@@ -49,40 +48,49 @@ def fetch_movie_details(record, api_key=dlt.secrets["tmdb_access_token"]):
     movie_id = record["id"]
     
     # Construcción de la request
-    url = f"{MOVIE_URL}/{movie_id}"
+    # Construcción de la request
+    # MOVIE_URL termina en /, así que concatenamos directamente
+    url = f"{MOVIE_URL}{movie_id}?append_to_response=credits"
     params = {
         "api_key": api_key,
         "language": "es-ES" # Opcional: traer datos en español
     }
 
     # dlt maneja los reintentos si la API devuelve 429
-    response = dlt_requests.get(url, params=params)
-    
-    if response.status_code == 200:
+    try:
+        response = dlt_requests.get(url, params=params)
+        response.raise_for_status() # Aseguramos que se lance excepción si no es 200 OK
+        
         # Devolvemos el JSON completo de la película enriquecida
         yield response.json()     
-    else:
-        # Opcional: Manejo de errores suaves (soft errors)
-        print(f"Error fetching ID {movie_id}: {response.status_code}")
+
+    except HTTPError as e:
+        if e.response.status_code == 404:
+            print(f"Movie ID {movie_id} not found (404). Skipping.")
+            return # Saltamos este registro silenciosamente
+        else:
+            print(f"Error fetching ID {movie_id}: {e}")
+            raise e # Relanzamos otros errores para que dlt los maneje (ej. 500)
 
 # 3. EJECUCIÓN DEL PIPELINE CON PARALELISMO
 # Utiliza DB_PATH definido en config.py (Fuente única de verdad)
 
 pipeline = dlt.pipeline(
-    pipeline_name="tmdb_enrichment_pipeline",
+    pipeline_name="tmdb_get_all_movies_pipeline",
     destination=duckdb(DB_PATH), 
-    dataset_name="tmdb_data"
+    dataset_name="raw_movies",
+    import_schema_path=DLT_SCHEMA_PATH+"/import",
+    export_schema_path=DLT_SCHEMA_PATH+"/export"
 )
 
 # Aquí es donde ocurre la MAGIA de la concurrencia.
 # El pipe (|) conecta los recursos.
 
 logger = setup_logger(
-    __name__, 
-    log_file=LOG_FILE, 
-    capture_external_loggers=["dlt"]  # Captura logs de dlt
-    )
+            __name__, 
+            log_file=LOG_FILE, 
+            capture_external_loggers=["dlt"]  # Captura logs de dlt
+            )
 logger.info("Iniciando pipeline de primer ingesta")
 pipeline.run(tmdb_daily_ids_stream | fetch_movie_details)
 logger.info("Pipeline de primer ingesta completado")
-
