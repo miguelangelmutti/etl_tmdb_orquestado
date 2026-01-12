4d937e39b45bac416eb3488b9a4034ce1ca6d20d
Migue <miguelangelmutti@hotmail.com>
2026-01-12 20:24:13 -0300

    cambio logica de la fact movies daily measures

--- transform/models/marts/fact_movies_daily_measures.sql
+++ transform/models/marts/fact_movies_daily_measures.sql
@@ -1,8 +1,8 @@
 {{
     config(
         materialized='incremental',
-        unique_key=['dim_movie_id'],
-        incremental_strategy='merge'
+        unique_key=['dim_movie_id','fact_date'],
+        incremental_strategy='append'
     )
 }}
 with last_movies as (
@@ -54,29 +54,12 @@ select
     popularity,
     vote_average,
     vote_count,        
-    {% if is_incremental() %}CURRENT_DATE as fact_date{% else %}dbt_valid_from::DATE as fact_date{% endif %}
+    dbt_valid_from::DATE as fact_date
 from last_movies
 {% if is_incremental() %} 
 where rn = 1
 {% endif %}
 )
-{% if is_incremental() %}
-,
-last_snapshot AS (
-        SELECT
-        ls.dim_movie_id,
-        lm.tmdb_id,        
-        ls.budget,
-        ls.revenue,
-        ls.popularity,
-        ls.vote_average,
-        ls.vote_count,        
-        ls.fact_date
-    FROM {{ this }} ls 
-        JOIN {{ref('int_last_movie_changes_snapshot')}} lm ON ls.dim_movie_id = lm.dbt_scd_id
-    WHERE ls.fact_date = (SELECT MAX(fact_date) FROM {{ this }})
-)
-{% endif %}
 SELECT
     n.dim_movie_id,    
     n.fact_date,
@@ -87,12 +70,5 @@ SELECT
     n.revenue
 FROM new_movies_measures n
 {% if is_incremental() %}
-LEFT JOIN last_snapshot l ON n.tmdb_id = l.tmdb_id
-WHERE
-    l.tmdb_id IS NULL
-    OR n.vote_count != l.vote_count
-    OR n.vote_average != l.vote_average
-    OR n.budget != l.budget
-    OR n.revenue != l.revenue
-    OR ABS(n.popularity - l.popularity) > 0.01
+where n.fact_date > (SELECT MAX(fact_date) FROM {{ this }})
 {% endif %}
