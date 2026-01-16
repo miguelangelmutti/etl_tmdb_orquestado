565e32596427e906c2fcd3f98373e75772d6cecd
Migue <miguelangelmutti@hotmail.com>
2026-01-16 20:01:27 -0300

    agrego dag para ingesta de exports

--- orchestration/dags/pipeline_tmdb_daily_exports.py
+++ orchestration/dags/pipeline_tmdb_daily_exports.py
@@ -8,20 +8,20 @@ from datetime import datetime
 HOST_PROJECT_PATH = os.getenv("HOST_PROJECT_PATH")
 
 with DAG(
-    'dag_tmdb_changes',
+    'dag_tmdb_daily_exports',
     start_date=datetime(2024, 1, 1),
     schedule_interval=None,
     catchup=False
 ) as dag:
 
-    ingestar_cambios_api = DockerOperator(
-        task_id='ingestar_cambios_api',
+    ingestar_daily_exports = DockerOperator(
+        task_id='ingestar_daily_exports',
         image='python:3.11-slim',
         api_version='auto',
         auto_remove=True,
         # 1. Install dependencies from the mounted requirements file
         # 2. Run the ingestion script
-        command="/bin/bash -c 'pip install -r ingestion/requirements.txt && python ingestion/dlthub_api_consumer.py'",
+        command="/bin/bash -c 'pip install -r ingestion/requirements.txt && python ingestion/dlt_export_test.py'",
         docker_url='unix://var/run/docker.sock',
         network_mode='etl-network',  # Connect to the same network as other services
         mounts=[
@@ -38,13 +38,14 @@ with DAG(
         }
     )
 
-    transformar_cambios_db = DockerOperator(
-        task_id='transformar_cambios_db',
+    transformar_daily_exports = DockerOperator(
+        task_id='transformar_daily_exports',
         image='python:3.11-slim',
         api_version='auto',
         auto_remove=True,
-        # 1. Install dependencies from the mounted requirements file
-        # 2. Run the dbt build script
+        # 1. Generate dynamic profiles.yml
+        # 2. Install dependencies
+        # 3. Run dbt build using the generated profile
         command="""/bin/bash -c 'mkdir -p ~/.dbt && echo "movies_profile:
   target: dev
   outputs:
@@ -52,19 +53,19 @@ with DAG(
       type: duckdb
       path: /app/database/shared_movies.duckdb" > ~/.dbt/profiles.yml && pip install -r transform/requirements.txt && cd transform && dbt build --profiles-dir ~/.dbt'""",
         docker_url='unix://var/run/docker.sock',
-        network_mode='etl-network',  # Connect to the same network as other services
+        network_mode='etl-network',
         mounts=[
             Mount(
-                source=HOST_PROJECT_PATH,  # Absolute path on the HOST machine
-                target="/app",             # Path inside the container
+                source=HOST_PROJECT_PATH,
+                target="/app",
                 type="bind"
             )
         ],
         working_dir="/app",
         environment={
-            "TOKEN": os.getenv("TOKEN"),     # Pass TMDB Token if needed explicitly
-            "API_KEY": os.getenv("API_KEY")  # Pass API Key if needed explicitly
+            "TOKEN": os.getenv("TOKEN"),
+            "API_KEY": os.getenv("API_KEY")
         }
     )
 
-    ingestar_cambios_api >> transformar_cambios_db
+    ingestar_daily_exports >> transformar_daily_exports
